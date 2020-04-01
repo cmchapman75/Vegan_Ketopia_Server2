e286cc2d69dc8d10a5a0bc5ebed119cbcb1d5565
Christina Chapman <cmchapman75@gmail.com>
2020-04-01 17:57:45 -0500

    Updating files in git for server

--- src/config.js
+++ src/config.js
@@ -7,6 +7,7 @@ module.exports = {
   TEST_DATABASE_URL:
           process.env.TEST_DATABASE_URL ||
           "postgresql://dunder_mifflin:mypassword@localhost/vegan_ketopia_test",
-  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
+  JWT_SECRET: process.env.JWT_SECRET || "vegan-keptoia-secret",
+//   JWT_EXPIRY: process.env.JWT_EXPIRY || '12h',
 };
       
